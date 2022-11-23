4cf07129049c2de37890dd45c3ca783b74bcb28f
Jakub <jakub.bchyla@gmail.com>
2022-11-23 05:44:53 +0100

    Initial commit

---
+++ src/main/java/com/jchyla/javaguidsonlyform/JavaguidsonlyformApplication.java
@@ -0,0 +1,13 @@
+package com.jchyla.javaguidsonlyform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaguidsonlyformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaguidsonlyformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jchyla/javaguidsonlyform/JavaguidsonlyformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jchyla.javaguidsonlyform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaguidsonlyformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
